0dfa7f1c6e2b53757a6b1d49130a410d00ce4efc
srodgers <steve@rodgers619.com>
2021-09-04 15:49:11 -0700

    Add display class

---
+++ include/display.hpp
@@ -0,0 +1,23 @@
+#ifndef __DISPLAY_HPP__
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <event.hpp>
+
+class DISPLAY_DRIVER
+{
+    public:
+    void begin();
+    void refresh();
+    void events(event_data ed, uint8_t event_subtype);
+
+    private:
+    uint32_t freq;
+    uint8_t mode;
+    uint8_t tx_mode;
+    
+    
+};
+
+#define __DISPLAY_HPP__
+
+#endif
---
+++ src/display.cpp
@@ -0,0 +1,92 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#include <config.hpp>
+#include <display.hpp>
+#include <event.hpp>
+
+
+
+U8G2_ST7920_128X64_1_HW_SPI st7920(U8G2_R0, PIN_SPI_CS, U8X8_PIN_NONE);
+
+//
+// Initialize the display
+//
+
+void DISPLAY_DRIVER::begin()
+{
+    st7920.begin();
+
+}
+
+//
+// Refresh the display
+//
+
+void DISPLAY_DRIVER::refresh()
+{
+    const char *modestr;
+    const char *radiostr;
+
+    uint32_t mhz = freq/1000000UL;
+    uint32_t modulus = freq % 1000000UL;
+
+    String fmhz(mhz);
+    String fmod(modulus);
+    String fmhzdot(fmhz + ".");
+    String freqall(fmhzdot+fmod);
+
+
+    if(mode == MODE_USB)
+        modestr = "USB";
+    else
+        modestr = "LSB";
+    
+    switch(tx_mode){
+        case RADIO_RX:
+            radiostr = "RX";
+            break;
+        case RADIO_TX:
+            radiostr = "TX";
+            break;
+        case RADIO_TUNE:
+            radiostr = "TU";
+            break;
+        default:
+            radiostr = "";
+    }
+
+    st7920.firstPage();
+    do {
+        /* all graphics commands have to appear within the loop body. */    
+        st7920.setFont(u8g2_font_ncenB14_tr);
+        st7920.drawStr(0, 20, freqall.c_str());
+        st7920.setFont(u8g2_font_ncenB08_tr);
+        st7920.drawStr(100, 20, modestr);
+        st7920.drawStr(100, 60, radiostr);
+    } while ( st7920.nextPage() );
+
+}
+
+//
+// Called when there is something to update
+//
+
+void DISPLAY_DRIVER::events(event_data ed, uint8_t event_subtype)
+{
+    switch(event_subtype){
+        case EV_SUBTYPE_SET_FREQ:
+            freq = ed.u32_val;
+            break;
+        case EV_SUBTYPE_SET_MODE:
+            mode = ed.u8_val;
+            break;
+        case EV_SUBTYPE_TX_MODE:
+            tx_mode = ed.u8_val;
+            break;
+        default:
+            break;
+    }
+
+
+}
